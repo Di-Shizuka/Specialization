aa2b96e7c9dba186f5128a126cbb25d0858eb904
ErmolovaDiana <ms.di.diana34@gmail.com>
2023-04-01 05:49:44 +0300

    add solving

--- HW/Program.cs
+++ HW/Program.cs
@@ -1,1 +1,27 @@
 ﻿
+void NewArray(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[j] = array1[i];
+            j++;
+        }
+    }
+}
+
+void PrintArray(string[] array2)
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        Console.Write($"{array2[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array1 = new string[4] { "Hello", "2", "world", ":-)" };
+string[] array2 = new string[array1.Length];
+NewArray(array1, array2);
+PrintArray(array2);
